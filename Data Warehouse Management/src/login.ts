3f41104f64a4e2824aa924f367455af3a8e928f0
Rahul garg <60229982+rahulgarg55@users.noreply.github.com>
2023-08-09 18:31:55 +0530

    Delete Data Warehouse Management/src directory

--- Data Warehouse Management/src/login.ts
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    const loginform=document.getElementById('login-form') as HTMLFormElement;
-    loginform.addEventListener('submit',(event)=>{
-        event.preventDefault();
-        const username=(loginform.elements.namedItem('username') as HTMLInputElement).value;
-        const password=(loginform.elements.namedItem('password') as HTMLInputElement).value;
-
-    })
-})
